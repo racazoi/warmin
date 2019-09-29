055feb2e70bddda02764702e07cfbbaa127e44b8
ismail Özacar <aziz@ismail-MacBook-Pro.local>
2019-09-29 17:28:22 +0300

    Complete first commit

---
+++ warmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  warmin
+//
+//  Created by ismail Özacar on 28.09.2019.
+//  Copyright © 2019 Kubit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
